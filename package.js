51e7da4952388b37ddca9d57d44bdddb33d811c6
Aram Kocharyan <akarmenia@gmail.com>
2014-11-14 12:27:20 +1100

    Update package info.

--- package.js
+++ package.js
@@ -1,13 +1,14 @@
 Package.describe({
-  name: 'collection-table',
+  name: 'aramk:collection-table',
   version: '0.1.0',
-  summary: 'A CRUD table template for displaying collection items in Meteor.'
+  summary: 'A CRUD table template for displaying collection items in Meteor.',
+  git: 'https://github.com/aramk/meteor-collection-table.git'
 });
 
 Package.on_use(function(api) {
-  api.use(['templating', 'underscore', 'jquery', 'less', 'reactive-table', 'aramk:utility'],
-      'client');
-
+  api.versionsFrom('METEOR@0.9.0');
+  api.use(['templating', 'underscore', 'jquery', 'less', 'aslagle:reactive-table@0.4.2',
+    'aramk:utility@0.3.0'], 'client');
   api.add_files([
     'lib/collectionTable.html',
     'lib/collectionTable.js',
