6f495d5c1d8634fff865378759ca6fa32dce80dd
Aram Kocharyan <akarmenia@gmail.com>
2014-12-08 12:18:09 +1100

    Migrated usage of session variables and table IDs to reactive variables and DOM elements.

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   name: 'aramk:collection-table',
-  version: '0.1.0',
+  version: '0.2.0',
   summary: 'A CRUD table template for displaying collection items in Meteor.',
   git: 'https://github.com/aramk/meteor-collection-table.git'
 });
 
 Package.on_use(function(api) {
   api.versionsFrom('METEOR@0.9.0');
-  api.use(['templating', 'underscore', 'jquery', 'less', 'aslagle:reactive-table@0.4.2',
-    'aramk:utility@0.3.0'], 'client');
+  api.use(['templating', 'underscore', 'jquery', 'less', 'reactive-var@1.0.3',
+    'aslagle:reactive-table@0.4.2', 'aramk:utility@0.3.0'], 'client');
   api.add_files([
     'lib/collectionTable.html',
     'lib/collectionTable.js',
