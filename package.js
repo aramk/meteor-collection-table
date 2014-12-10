a49e0b4c378926f89d96bd937058c8790b5c2e44
Aram Kocharyan <akarmenia@gmail.com>
2014-12-10 23:38:45 +1100

    Fixed issues with "collection" being used instead of "items" when provided.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'aramk:collection-table',
-  version: '0.2.0',
+  version: '0.2.1',
   summary: 'A CRUD table template for displaying collection items in Meteor.',
   git: 'https://github.com/aramk/meteor-collection-table.git'
 });
