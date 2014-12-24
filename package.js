57d15dc3e8602d95553331da809d5206dd42d4f8
Aram Kocharyan <akarmenia@gmail.com>
2014-12-24 13:11:27 +1100

    Updated getRow() to prevent needing to use a custom fork of reactive-table which adds a "data-id" attribute to row items. Version bump.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'aramk:collection-table',
-  version: '0.2.2',
+  version: '0.2.3',
   summary: 'A CRUD table template for displaying collection items in Meteor.',
   git: 'https://github.com/aramk/meteor-collection-table.git'
 });
