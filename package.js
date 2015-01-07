d214d7ffa509d0040b29b5dc2607c15b22cb3e63
Aram Kocharyan <akarmenia@gmail.com>
2015-01-07 13:02:30 +1100

    Added dependency on font-awesome. Version bump.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'aramk:collection-table',
-  version: '0.2.4',
+  version: '0.2.5',
   summary: 'A CRUD table template for displaying collection items in Meteor.',
   git: 'https://github.com/aramk/meteor-collection-table.git'
 });
@@ -8,7 +8,8 @@ Package.describe({
 Package.on_use(function(api) {
   api.versionsFrom('METEOR@0.9.0');
   api.use(['templating', 'underscore', 'jquery', 'less', 'reactive-var@1.0.3',
-    'aslagle:reactive-table@0.4.2', 'aramk:utility@0.3.0'], 'client');
+    'aslagle:reactive-table@0.4.2', 'aramk:utility@0.3.0', 'natestrauser:font-awesome@4.2.0'],
+    'client');
   api.add_files([
     'lib/collectionTable.html',
     'lib/collectionTable.js',
