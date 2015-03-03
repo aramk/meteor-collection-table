635b4c4f023f634ce187ad22b2a4aba934ef5043
Aram Kocharyan <akarmenia@gmail.com>
2015-03-03 15:47:48 +1100

    Version bump.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'aramk:collection-table',
-  version: '0.3.2',
+  version: '0.3.3',
   summary: 'A CRUD table template for displaying collection items in Meteor.',
   git: 'https://github.com/aramk/meteor-collection-table.git'
 });
