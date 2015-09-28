24336f21386fbfd0222c6c253a54a295511748cd
Aram Kocharyan <akarmenia@gmail.com>
2015-09-28 10:16:39 +1000

    Support for Meteor 1.2

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'aramk:collection-table',
-  version: '0.4.0',
+  version: '0.4.1',
   summary: 'A CRUD table template for displaying collection items in Meteor.',
   git: 'https://github.com/aramk/meteor-collection-table.git'
 });
 
 Package.on_use(function(api) {
-  api.versionsFrom('METEOR@0.9.0');
+  api.versionsFrom('METEOR@1.2.0.1');
   api.use([
     'coffeescript',
     'templating',
@@ -15,7 +15,7 @@ Package.on_use(function(api) {
     'less',
     'reactive-var@1.0.3',
     'aslagle:reactive-table@=0.8.1',
-    'urbanetic:utility@1.0.0'
+    'urbanetic:utility@1.1.0'
   ], 'client');
   api.use(['iron:router@1.0.7'], 'client', {weak: true});
   api.add_files([
