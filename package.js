351bdc73c728ae9d44c5a78c1cf0c5f11617bbca
Aram Kocharyan <akocharyan@fb.com>
2018-02-26 00:59:09 -0800

    Meteor. 1.6.1 support

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'aramk:collection-table',
-  version: '0.5.0',
+  version: '1.0.0',
   summary: 'A CRUD table template for displaying collection items in Meteor.',
   git: 'https://github.com/aramk/meteor-collection-table.git'
 });
 
 Package.on_use(function(api) {
-  api.versionsFrom('METEOR@1.2.0.1');
+  api.versionsFrom('METEOR@1.6.1');
   api.use([
     'coffeescript',
     'templating',
@@ -17,7 +17,7 @@ Package.on_use(function(api) {
     'aslagle:reactive-table@0.8.1',
     'matb33:collection-hooks@0.8.0',
     'reactive-var@1.0.3',
-    'urbanetic:utility@1.2.0'
+    'urbanetic:utility@2.0.0'
   ], 'client');
   api.use(['iron:router@1.0.13'], 'client', {weak: true});
   api.add_files([
